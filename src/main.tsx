550394ca8fbf7729420d20d6b09b1b4a3d3310c5
daovantan <youandi852003@gmail.com>
2024-10-20 21:13:55 +0700

    fdf

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client'
 import Home from './Home.tsx'
 
 
-const params = new URLSearchParams(Telegram.WebApp.initData);
-
-const userData = Object.fromEntries(params);
-// userData.user = JSON.parse(userData.user);
-
-console.log(userData);
-
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
