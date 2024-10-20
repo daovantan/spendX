d63c598a298d2009e3b7df481f5cf6dab7996891
daovantan <youandi852003@gmail.com>
2024-10-20 21:35:20 +0700

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  let port = 3000;
-  if (+process.env.APP_PORT) {
-    port = +process.env.APP_PORT;
-  }
+  // let port = 3000;
+  // if (+process.env.APP_PORT) {
+  //   port = +process.env.APP_PORT;
+  // }
   return {
     plugins: [react()],
-    server: {
-      port: port,
-    }
+    // server: {
+    //   port: port,
+    // }
   }
 })
