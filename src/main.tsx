df6a15cc6e5ca4758fca0e549325556aa0cf93eb
daovantan <youandi852003@gmail.com>
2024-10-22 19:00:01 +0700

    dà

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,30 @@ import ReactDOM from 'react-dom/client'
 import Home from './Home.tsx'
 
 
+const overflow = 100
+document.body.style.overflowY = 'hidden'
+document.body.style.marginTop = `${overflow}px`
+document.body.style.height = window.innerHeight + overflow + "px"
+document.body.style.paddingBottom = `${overflow}px`
+window.scrollTo(0, overflow)
 
+let ts: number | undefined
+const onTouchStart = (e: TouchEvent) => {
+  ts = e.touches[0].clientY
+}
+const onTouchMove = (e: TouchEvent) => {
+  if (scrollableEl) {
+    const scroll = scrollableEl.scrollTop
+    const te = e.changedTouches[0].clientY
+    if (scroll <= 0 && ts! < te) {
+      e.preventDefault()
+    }
+  } else {
+    e.preventDefault()
+  }
+}
+document.documentElement.addEventListener('touchstart', onTouchStart, { passive: false })
+document.documentElement.addEventListener('touchmove', onTouchMove, { passive: false })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
