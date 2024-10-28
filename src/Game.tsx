01a32b5302e6479f41ccb8b63ad384687c51f10d
daovantan <youandi852003@gmail.com>
2024-10-28 14:31:08 +0700

    11

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,26 +3,6 @@ import AnimateCC from "react-adobe-animate";
 
 const App: React.FC = () => {
   
-//Dig coins.
-const [digCoins, setDigCoins] = useState(1);
-const [iconDig, setIconDig] = useState("./src/images/gif/icon_dig.gif");
-
-useEffect(() => {
-  var isStop = false;
-  const interval = setInterval(() => {
-    if(digCoins >= 100){
-      if(!isStop) {
-        setIconDig("./src/images/icon_dig.png");
-        console.log("stop dig coins - max=" + digCoins);
-      }
-      isStop = true;
-    }else{
-      setDigCoins(digCoins + 1);
-    }
-  }, 1000);
-  return () => clearInterval(interval);
-}, [digCoins]);
-
 
 window.Telegram.WebApp.ready();
 window.Telegram.WebApp.expand();
@@ -38,12 +18,6 @@ const debug = initData + " - "+ initDataUnsafe;
     <div className="app gamePage">
       <div className='appContent'>
         <p className='debug'>{debug}</p>
-
-        <div className='pinAnim' style={{ width: "400px" }}>
-          <AnimateCC
-            // animationName="pig_anim"
-          />
-        </div>
 
       </div>
     </div>
