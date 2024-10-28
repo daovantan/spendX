9cfeeddf5542776e498f42a67dcd5f4d284a86de
daovantan <youandi852003@gmail.com>
2024-10-28 16:25:18 +0700

    44

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,16 +2,9 @@
 
 const App: React.FC = () => {
   
-
-window.Telegram.WebApp.ready();
-//Telegram.WebApp.expand();
-// initData is empty object
-let initData = window.Telegram.WebApp.initData;
- // initDataUnsafe is empty object,too
-let initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-
 //const initData = window.Telegram.WebApp; //JSON.stringify(window.Telegram.WebApp);
-const debug = initData + " - "+ initDataUnsafe;
+const debug = Telegram.WebApp.initData + " - "+ Telegram.WebApp.initDataUnsafe;
+
 
   return (
     <div className="app gamePage">
