5610fccb2b7fd2ae33d9299fcdf9b3cfea54e55b
daovantan <youandi852003@gmail.com>
2024-10-28 14:40:39 +0700

    22

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import AnimateCC from "react-adobe-animate";
+
 
 const App: React.FC = () => {
   
 
-window.Telegram.WebApp.ready();
-window.Telegram.WebApp.expand();
+Telegram.WebApp.ready();
+//Telegram.WebApp.expand();
 // initData is empty object
-let initData = window.Telegram.WebApp.initData;
+let initData = Telegram.WebApp.initData;
  // initDataUnsafe is empty object,too
-let initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
+let initDataUnsafe = Telegram.WebApp.initDataUnsafe;
 
 //const initData = window.Telegram.WebApp; //JSON.stringify(window.Telegram.WebApp);
 const debug = initData + " - "+ initDataUnsafe;
